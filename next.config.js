d6977e06eb7b604e7105fddf2eb995e920a3ea16
SlKzᵍᵐ <105301169+slkzgm@users.noreply.github.com>
2022-06-16 21:35:55 +0200

    Force https 2nd attemps

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  headers: [
+    {
+      key: 'Strict-Transport-Security',
+      value: 'max-age=630720000; includeSubDomains; preload'
+    },
+    {
+      key: 'X-DNS-Prefetch-Control',
+      value: 'on'
+    },
+    {
+      key: 'X-XSS-Protection',
+      value: '1; mode=block'
+    }
+  ]
 }
 
 module.exports = nextConfig
