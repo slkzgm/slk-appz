3bfe647a22f61645223636157c83fc214a462875
SlKzᵍᵐ <105301169+slkzgm@users.noreply.github.com>
2022-06-16 22:10:37 +0200

    3rd attemps https forcing

--- next.config.js
+++ next.config.js
@@ -1,20 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  headers: [
-    {
-      key: 'Strict-Transport-Security',
-      value: 'max-age=630720000; includeSubDomains; preload'
-    },
-    {
-      key: 'X-DNS-Prefetch-Control',
-      value: 'on'
-    },
-    {
-      key: 'X-XSS-Protection',
-      value: '1; mode=block'
-    }
-  ]
+  async headers() {
+    return [{
+      source: '/:path*',
+      headers: [
+        {
+          key: 'Strict-Transport-Security',
+          value: 'max-age=630720000; includeSubDomains; preload'
+        },
+        {
+          key: 'X-DNS-Prefetch-Control',
+          value: 'on'
+        },
+        {
+          key: 'X-XSS-Protection',
+          value: '1; mode=block'
+        }]
+    }];
+  }
 }
 
 module.exports = nextConfig
