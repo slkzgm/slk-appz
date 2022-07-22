a650dbaaac0e4f13d74c28df3925b12b8680b5f7
SlKzGM <slkzgm@github.com>
2022-07-22 20:24:30 +0200

    Re-Enable API use

--- pages/mnlth.js
+++ pages/mnlth.js
@@ -7,18 +7,13 @@ import {Box, Button, Typography} from "@mui/material";
 import {roundToTwo} from "../lib/utils";
 
 export async function getServerSideProps(context) {
-  // const data = await requestMnlthAPI('floor');
-  const data = {};
+  const data = await requestMnlthAPI('floor');
 
   return {
     props: {
       data
     }
   };
-}
-
-function Link(props) {
-  return null;
 }
 
 export default function Mnlth({ data }) {
@@ -37,26 +32,26 @@ export default function Mnlth({ data }) {
 
   return (
     <Layout>
-      <Typography variant={'body2'} paragraph={true} textAlign={'center'}>
-        Temporarily unavailable as the scrapping script needs to be updated and I'm not available until the end of the month. <br/>
-        Please, see the link below for less detailed live data. <br/>
-        My apologizes for the inconvenience.
-      </Typography>
-      <Box sx={{textAlign: 'center', margin: '.5rem'}}>
-        <Button variant={'contained'} color={'error'}>
-          <a href={"https://docs.google.com/spreadsheets/d/1MfLZU3EKSBlbNm1v_eSMvd1kCjMugu9wAXI4Fdvq-kE/edit?usp=sharing"} target={'_blank'} rel="noopener noreferrer">
-            <Typography variant={'button'} fontWeight={'bold'}>
-              GSheet MNLTH live data
-            </Typography>
-          </a>
-        </Button>
-      </Box>
-      {/*<MnlthButton activeCommand={stateCommand} handleClick={handleClick}/>*/}
-      {/*<MnlthTable data={stateData} />*/}
-      {/*{*/}
-      {/*  stateCommand === 'opening' ?*/}
-      {/*    <Typography sx={{margin: '.5rem', textAlign: 'center'}}>Max value loss: {roundToTwo(stateData[9].maxLoss)} ETH. (-{roundToTwo(stateData[9].maxLossPercent)}%)</Typography> : ''*/}
-      {/*}*/}
+      {/*<Typography variant={'body2'} paragraph={true} textAlign={'center'}>*/}
+      {/*  Temporarily unavailable as the scrapping script needs to be updated and I'm not available until the end of the month. <br/>*/}
+      {/*  Please, see the link below for less detailed live data. <br/>*/}
+      {/*  My apologizes for the inconvenience.*/}
+      {/*</Typography>*/}
+      {/*<Box sx={{textAlign: 'center', margin: '.5rem'}}>*/}
+      {/*  <Button variant={'contained'} color={'error'}>*/}
+      {/*    <a href={"https://docs.google.com/spreadsheets/d/1MfLZU3EKSBlbNm1v_eSMvd1kCjMugu9wAXI4Fdvq-kE/edit?usp=sharing"} target={'_blank'} rel="noopener noreferrer">*/}
+      {/*      <Typography variant={'button'} fontWeight={'bold'}>*/}
+      {/*        GSheet MNLTH live data*/}
+      {/*      </Typography>*/}
+      {/*    </a>*/}
+      {/*  </Button>*/}
+      {/*</Box>*/}
+      <MnlthButton activeCommand={stateCommand} handleClick={handleClick}/>
+      <MnlthTable data={stateData} />
+      {
+        stateCommand === 'opening' ?
+          <Typography sx={{margin: '.5rem', textAlign: 'center'}}>Max value loss: {roundToTwo(stateData[9].maxLoss)} ETH. (-{roundToTwo(stateData[9].maxLossPercent)}%)</Typography> : ''
+      }
     </Layout>
   );
 }
