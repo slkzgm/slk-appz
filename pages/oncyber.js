a2c11d2996cebfeed32b2d8510de8cae6233a8b2
SlKzᵍᵐ <105301169+slkzgm@users.noreply.github.com>
2023-02-01 19:42:39 +0100

    Skin update for /oncyber

--- pages/oncyber.js
+++ pages/oncyber.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import Layout from "../components/Layout";
-import {Box, TextField, Typography} from "@mui/material";
+import {Box, Button, CardMedia, Divider, Grid, TextField, Typography} from "@mui/material";
 import {requestMetaTagAPI} from "../lib/metaTags";
 import LoadingButton from '@mui/lab/LoadingButton';
 import {requestOncyberArtworksList} from "../lib/oncyber";
@@ -16,21 +16,38 @@ export async function getServerSideProps(context) {
   };
 }
 
-const ArtworksDisplay = ({ artworks }) => <Box>
-  {artworks.map((artwork, index) =>
-    <div key={index}>
-      {Object.keys(artwork).map(field => {
-        if (field === 'name')
-          return <Typography color={'primary'}>{artwork[field]}</Typography>
-        if (field === 'mimeType')
-          return <Typography>{artwork[field]}</Typography>
-        if (artwork[field])
-          return <a href={artwork[field]} target={'_blank'} rel="noopener noreferrer">
-            <Typography>{field}</Typography>
-          </a>
-      })}
-    </div>)}
-</Box>
+const ArtworkDisplay = ({artwork, index}) =>
+  <Grid key={index} container spacing={2} justifyContent={'center'} alignItems={'center'}
+    sx={{textAlign: 'center', margin: '1rem', padding: '0.5rem', borderRadius: '2%',
+      }}
+  >
+    <Grid item xs={6}>
+      <CardMedia
+        component={'img'}
+        image={artwork.image_preview_url}
+        sx={{height: '100%', borderRadius: '2%'}}
+      />
+    </Grid>
+    <Grid item xs={6}>
+      <Grid item xs={12}>
+        <Typography variant={'primary'} color={'primary'}>{artwork.name}</Typography>
+        <Typography>{artwork.mimeType}</Typography>
+      </Grid>
+      <Divider sx={{margin: '1rem'}}/>
+      <Grid item xs={12} zeroMinWidth>
+        {Object.keys(artwork).map(field => {
+          if (artwork[field] && field !== 'name' && field !== 'mimeType') {
+            return <a href={artwork[field]} target={'_blank'} rel="noopener noreferrer">
+              <Typography>{field}</Typography>
+            </a>
+          }
+        })}
+      </Grid>
+    </Grid>
+  </Grid>
+
+const ArtworksDisplay = ({ artworks }) =>
+  <Box>{artworks.map((artwork, index) => <ArtworkDisplay artwork={artwork} index={index}/>)}</Box>
 
 export default function Oncyber({ metaTags }) {
   const [spaceId, setSpaceId] = useState("");
@@ -47,6 +64,11 @@ export default function Oncyber({ metaTags }) {
 
   return (
     <Layout metaTags={metaTags}>
+      <CardMedia
+        component={'img'}
+        image={'https://www.slkzgm.com/public/img/oncyber_tutorial.png'}
+        sx={{height: '100%', borderRadius: '2%'}}
+      />
       <Box
         sx={{
           margin: '.5rem',
