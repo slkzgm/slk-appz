495fc8a9d53e63ba2f276a682364a8c60c3b6bea
SlKzᵍᵐ <105301169+slkzgm@users.noreply.github.com>
2023-02-16 19:12:01 +0100

    Adding copy to clipboard IconButton to /blur

--- pages/blur.js
+++ pages/blur.js
@@ -1,6 +1,18 @@
 import React, {useState} from "react";
 import Layout from "../components/Layout";
-import {Box, CardMedia, TextField, Typography} from "@mui/material";
+import {
+  Box,
+  Button,
+  CardMedia,
+  Divider,
+  Grid,
+  IconButton,
+  Paper,
+  TextField,
+  Typography
+} from "@mui/material";
+import ContentPasteRoundedIcon from '@mui/icons-material/ContentPasteRounded';
+import InventoryRoundedIcon from '@mui/icons-material/InventoryRounded';
 import LoadingButton from "@mui/lab/LoadingButton";
 import {requestBlurAirdropInputs} from "../lib/blur";
 import {requestMetaTagAPI} from "../lib/metaTags";
@@ -15,15 +27,73 @@ export async function getServerSideProps(context) {
   };
 }
 
+const Block = ({label, value}) => {
+  const handleClick = () => {
+    navigator.clipboard.writeText(value);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 300);
+  };
+  const [copied, setCopied] = useState(false);
+
+  return (
+    <div style={{ width: '100%', margin: '.5rem'}}>
+      <Typography>{label}</Typography>
+      <Box
+        component="span"
+        sx={{
+          display: 'flex',
+          p: 1,
+          bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#101010' : '#fff'),
+          color: (theme) =>
+            theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
+          border: '1px solid',
+          borderColor: (theme) =>
+            theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
+          borderRadius: 2,
+        }}
+      >
+        <Grid container>
+          <Grid item xs={11}>
+            <Typography noWrap sx={{
+              fontSize: '0.875rem',
+              fontWeight: '700',
+            }}>
+              {value}
+            </Typography>
+          </Grid>
+          <Grid item xs={1}>
+            <IconButton onClick={handleClick}>
+              {copied ? <InventoryRoundedIcon fontSize={'small'} sx={{
+                fontSize: '0.75rem',
+                fontWeight: '700',
+              }}/> :
+              <ContentPasteRoundedIcon fontSize={'small'} sx={{
+                fontSize: '0.75rem',
+                fontWeight: '700',
+              }}/>}
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
+}
+
 const InputParamsDisplay = ({ params }) => <Box>
-  {params.account ?
-    <Box>
-      <Typography>account (address)</Typography>
-      <Typography>{params.account}</Typography>
-      <Typography>amount (uint256)</Typography>
-      <Typography>{params.amount}</Typography>
-      <Typography>proof (bytes32[])</Typography>
-      <Typography>{params.proof}</Typography>
+  {params.account ? <Box sx={{
+      padding: '.25rem',
+      marginTop: '1rem'
+    }}>
+      <Box>
+        <Typography>1. claim (0x3d13f874)</Typography>
+      </Box>
+      <Box>
+        <Block label={'account (address)'} value={params.account}/>
+        <Block label={'amount (uint256)'} value={params.amount}/>
+        <Block label={'proof (bytes32[])'} value={params.proof}/>
+      </Box>
     </Box>
     :
     ''
@@ -31,6 +101,7 @@ const InputParamsDisplay = ({ params }) => <Box>
 </Box>
 
 const BlurTutorial = () => <Box sx={{margin: '1rem'}}>
+  <Divider variant={'middle'}><Typography>Tutorial</Typography></Divider>
   <Typography>1) Right-Click on the 'OPEN CARE PACKAGES FOR $BLUR' button, and copy link</Typography>
   <Typography>2) Paste your link in the field above</Typography>
   <Typography>3) Go to the etherscan BlurAirdrop contract page: 0xF2d15C0A89428C9251d71A0E29b39FF1e86bce25</Typography>
