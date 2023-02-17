76ecf09f59cbd821afe09e4d73d61a6ae7aeffae
SlKzᵍᵐ <105301169+slkzgm@users.noreply.github.com>
2023-02-17 11:44:57 +0100

    Fixing build error with new Analytics module

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import {Analytics} from "@vercel/analytics/dist/react";
+import {Analytics} from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return <>
