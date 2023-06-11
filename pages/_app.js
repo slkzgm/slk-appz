cc6ee8e56064b1cbbe266f01d9e0270e0da7a865
SlKzᵍᵐ <105301169+slkzgm@users.noreply.github.com>
2023-06-12 00:23:31 +0200

    Onlyfans added

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import '../styles/globals.css'
 import {Analytics} from "@vercel/analytics/react";
 import MaintenanceBanner from "../components/MaintenanceBanner";
+import {useRouter} from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  const shouldDisplayMaintenanceBanner = router.pathname !== '/of';
+
   return <>
-    <MaintenanceBanner />
+    {shouldDisplayMaintenanceBanner && <MaintenanceBanner />}
     <Component {...pageProps} />
     <Analytics />
     </>
---
+++ pages/of.js
@@ -0,0 +1,13 @@
+import {Typography} from "@mui/material";
+import Image from "mui-image";
+
+export default function OnlyFans() {
+    // return <div>
+    // </div>
+    return <>
+        <Typography variant="h1" textAlign={'center'}>
+            Lol, nope.
+        </Typography>
+        <Image src={'/images/nose.jpg'} alt={'nose'}/>
+    </>
+}
